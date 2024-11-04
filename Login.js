237b4a63137982e56fc6b3c5beb86cf96b0f4a27
MissAV <sofia.sry7@gmail.com>
2024-11-05 03:59:58 +0700

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,35 @@
+// components/Login.js
+import React from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput style={styles.input} placeholder="Email" />
+      <TextInput style={styles.input} placeholder="Kata Sandi" secureTextEntry />
+      <Button title="Login" onPress={() => alert('Login Berhasil')} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+});
+
+export default Login;
