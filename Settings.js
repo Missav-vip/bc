dc75088118e4084fc6e07f22d8bee1fda1a9fc6e
MissAV <sofia.sry7@gmail.com>
2024-11-05 03:58:30 +0700

    Create Settings.js

---
+++ Settings.js
@@ -0,0 +1,24 @@
+// components/Settings.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Settings = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Ini adalah halaman Pengaturan.</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+  },
+});
+
+export default Settings;
