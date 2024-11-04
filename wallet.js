82c47d3e98bdb775c5f43b49b37a8b4b9d621c0e
MissAV <sofia.sry7@gmail.com>
2024-11-05 03:11:30 +0700

    Create wallet.js

---
+++ wallet.js
@@ -0,0 +1,29 @@
+// components/Wallet.js
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+const Wallet = ({ address }) => {
+    const [balance, setBalance] = useState(0);
+
+    const getBalance = async () => {
+        try {
+            const response = await axios.get(`https://api.blockchain.info/balance?active=${address}`);
+            setBalance(response.data[address].final_balance / 100000000); // Mengonversi satoshi ke BTC
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        getBalance();
+    }, [address]);
+
+    return (
+        <View>
+            <Text>Saldo Bitcoin: {balance} BTC</Text>
+        </View>
+    );
+};
+
+export default Wallet;
